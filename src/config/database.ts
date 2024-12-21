f18a18f827197cf46b3d7af46f3d9f291178854f
Chonwai <chonwaiun@gmail.com>
2024-12-21 17:34:30 +0800

    Test: Test the DB config.

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,36 +1,55 @@
+// import { DataSourceOptions } from 'typeorm';
+// import dotenv from 'dotenv';
+// import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+// dotenv.config();
+
+// const isProd = process.env.NODE_ENV === 'production';
+
+// const prodConfig: DataSourceOptions = {
+//     type: 'postgres',
+//     url: process.env.POSTGRES_PRISMA_URL,
+//     ssl: {
+//         rejectUnauthorized: false,
+//     },
+//     entities: ['src/models/**/*.ts'],
+//     migrations: ['src/migrations/**/*.ts'],
+//     synchronize: false,
+//     logging: ['query', 'error', 'schema', 'warn', 'info', 'log'],
+//     namingStrategy: new SnakeNamingStrategy(),
+// };
+
+// const devConfig: DataSourceOptions = {
+//     type: 'postgres',
+//     host: process.env.DB_HOST || 'localhost',
+//     port: parseInt(process.env.DB_PORT || '5432'),
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     entities: ['src/models/**/*.ts'],
+//     migrations: ['src/migrations/**/*.ts'],
+//     synchronize: false,
+//     logging: ['query', 'error', 'schema', 'warn', 'info', 'log'],
+//     namingStrategy: new SnakeNamingStrategy(),
+// };
+
+// export const databaseConfig: DataSourceOptions = isProd ? prodConfig : devConfig;
 import { DataSourceOptions } from 'typeorm';
 import dotenv from 'dotenv';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 dotenv.config();
 
-const isProd = process.env.NODE_ENV === 'production';
-
-const prodConfig: DataSourceOptions = {
-    type: 'postgres',
-    url: process.env.POSTGRES_PRISMA_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    },
-    entities: ['src/models/**/*.ts'],
-    migrations: ['src/migrations/**/*.ts'],
-    synchronize: false,
-    logging: ['query', 'error', 'schema', 'warn', 'info', 'log'],
-    namingStrategy: new SnakeNamingStrategy(),
-};
-
-const devConfig: DataSourceOptions = {
+export const databaseConfig: DataSourceOptions = {
     type: 'postgres',
     host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT || '5432'),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    entities: ['src/models/**/*.ts'],
+    entities: ['src/models/*.ts'],
     migrations: ['src/migrations/**/*.ts'],
     synchronize: false,
     logging: ['query', 'error', 'schema', 'warn', 'info', 'log'],
     namingStrategy: new SnakeNamingStrategy(),
 };
-
-export const databaseConfig: DataSourceOptions = isProd ? prodConfig : devConfig;
